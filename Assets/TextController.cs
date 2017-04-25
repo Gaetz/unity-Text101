b4ab333a841ba39e6e6add8f033c6bc17f45fff7
Gaetz (Emilie-i3) <gaetz@rufflerim.com>
2017-04-25 19:43:32 +0200

    Complete game text

--- Assets/TextController.cs
+++ Assets/TextController.cs
@@ -34,19 +34,19 @@ public class TextController : MonoBehaviour {
             case States.cell:
                 StateCell(); break;
             case States.mirror:
-                StateCell(); break;
+                StateMirror(); break;
             case States.sheets_0:
                 StateSheets0(); break;
             case States.lock_0:
-                StateCell(); break;
+                StateLock0(); break;
             case States.cell_mirror:
-                StateCell(); break;
+                StateCellMirror(); break;
             case States.sheets_1:
-                StateCell(); break;
+                StateSheets1(); break;
             case States.lock_1:
-                StateCell(); break;
+                StateLock1(); break;
             case States.freedom:
-                StateCell(); break;
+                StateFreedom(); break;
         }
     }
 
@@ -62,6 +62,14 @@ public class TextController : MonoBehaviour {
         {
             currentState = States.sheets_0;
         }
+        else if (Input.GetKeyDown(KeyCode.M))
+        {
+            currentState = States.mirror;
+        }
+        else if (Input.GetKeyDown(KeyCode.L))
+        {
+            currentState = States.lock_0;
+        }
     }
 
     void StateSheets0()
@@ -71,6 +79,88 @@ public class TextController : MonoBehaviour {
             "you guess !\n\n" +
             "Press R to Return\n";
         if (Input.GetKeyDown(KeyCode.R))
+        {
+            currentState = States.cell;
+        }
+    }
+
+    void StateMirror()
+    {
+        text.text = "The dirty old mirror on the wall seems loose.\n\n" +
+            "Press T to Take the mirror\n"+
+            "Press R to Return\n";
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            currentState = States.cell;
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            currentState = States.cell_mirror;
+        }
+    }
+
+    void StateLock0()
+    {
+        text.text = "This is one of those button locks. You have no idea what the " +
+            "combinaison is. You wish you could somehow see where the dirty " +
+            "firgerprints were. Maybe that would help.\n\n" +
+            "Press R to Return roaming your cell.\n";
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            currentState = States.cell;
+        }
+    }
+
+    void StateCellMirror()
+    {
+        text.text = "You are still in your cell and STILL want to escape. There are " +
+            "some dirty sheets on the bed, a mark where the mirror was," +
+            "and that pesky door is still there, and firmly locked.\n\n" +
+            "Press S to view Sheets\n" +
+            "Press L to look at the Lock\n";
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            currentState = States.sheets_1;
+        }
+        else if (Input.GetKeyDown(KeyCode.L))
+        {
+            currentState = States.lock_1;
+        }
+    }
+
+    void StateSheets1()
+    {
+        text.text = "Holding a mirror in your hand doesn't make the sheets look " +
+            "any better.\n\n" +
+            "Press R to Return\n";
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            currentState = States.cell_mirror;
+        }
+    }
+
+    void StateLock1()
+    {
+        text.text = "You carefully but your mirror through the bars, and turn it around " +
+            "so you can see the lock. You canjust make out fingerprints around " +
+            "the buttons. You press dirty buttons, and hear the click.\n\n" +
+            "Press O to Open\n"+
+            "Press R to Return\n";
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            currentState = States.freedom;
+        }
+        else if (Input.GetKeyDown(KeyCode.R))
+        {
+            currentState = States.cell_mirror;
+        }
+    }
+
+    void StateFreedom()
+    {
+        text.text = "You are FREE !\n\n" +
+            "Press P to Play again\n";
+        if (Input.GetKeyDown(KeyCode.P))
         {
             currentState = States.cell;
         }
